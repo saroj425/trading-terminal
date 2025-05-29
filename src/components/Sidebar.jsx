36d6d44af3e63d82c5ade79fe44594434e59a041
Saroj Kumar Pandey <saroj425@gmail.com>
2025-05-29 23:14:54 +0530

    changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,8 @@ const Sidebar = ({toggleExtraPanel}) => {
   return (
     // <div className="w-16 bg-[#1d1d1d] flex flex-col items-center p-2 space-y-4">
     <div className={`w-16 flex flex-col items-center p-2 space-y-4 ${isDarkMode ? 'bg-[#1f1f1f] text-white border-r border-gray-700' : 'bg-white text-black border-r border-gray-300'}`}>
-        <button><img src={logo} className='w-25 h-25'/></button>
+        {/* <button><img src={logo} className='w-25 h-25'/></button> */}
+        <button className='border-b border-white/20'><b>TRADE</b></button>        
         <button onClick={toggleExtraPanel}><FaBars/></button>
         <button>⚙️</button>
         {/* Add icons using react-icons or similar */}
